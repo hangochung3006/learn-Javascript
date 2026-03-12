38e15fb483df324d6396560b3487e535b5983a15
Hưng <hangochung3006@gmail.com>
2026-03-12 11:39:10 +0700

    Update 12/03/2026

--- bai-tap-js/bai5/main.js
+++ bai-tap-js/bai5/main.js
@@ -1,3 +1,6 @@
 let myName;
 myName = prompt("Tên bạn là gì?");
-console.log("Hello " + myName);
+console.log("Hello " + myName); //Cách 1
+console.log("Hello", myName); //Cách 2
+console.log(`Hello ${myName}`); //Cách 3
+console.log(`Hello %s`, myName); //Cách 4
---
+++ bai-tap-js/bai8/main.js
@@ -0,0 +1,7 @@
+let numberA = prompt("Mời bạn nhập số vào A: ");
+console.log(typeof numberA);
+let numberB = 10;
+console.log(typeof numberB);
+let tong = numberA + numberB;
+console.log("Tổng của A và B là: " + tong);
+console.log("Kiểu dữ liệu của tổng là: " + typeof tong);
